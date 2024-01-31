4077be9e1021a7c2909e367581e9e1dbff212aad
sibbe23 <subashssubramaniank@gmail.com>
2024-01-31 14:42:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Todo } from './rule/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'Todolist';
+
+  todoDescription: String = "";
+  todoDate:Date = new Date();
+
+  details: Todo []= []
+
+  display()
+    {
+    if(this.todoDescription.trim().length && this.todoDate)
+    {
+
+              let newTodo : Todo = 
+              {
+                id:Math.floor(Math.random()*100),
+                description:this.todoDescription,
+                date:this.todoDate
+                }
+
+          this.details.push(newTodo)
+
+        this.todoDescription="";
+        this.todoDate = new Date();
+    }
+    }
+
+  delete(index:number)
+    {
+      this.details.splice(index,1)
+    }
 }
